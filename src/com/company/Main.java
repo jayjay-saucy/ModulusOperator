e1fd9bc79591c300443c012275198a6decf73844
Jayllian Marquardt <jayllian.marquardt.2021@student.lhschools.org>
2020-03-03 14:04:59 -0500

    OddnessMethodCreated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    public static void testOdness(){
+        testOdness();
+
+    }
     public static void main(String[] args) {
-	// write your code here
+	// todo even or odd
+        // todo keep number contained
+        // todo get the last digit
+        //We want to find out if a number is even or odd
+        // is even ==
+        // is not even !=
+        System.out.println("Give me a number between 1-100. ");
+Scanner keyboard;
+
+    keyboard = new Scanner(System.in);
+    keyboard.nextLine();
+    int number = 17;
+    System.out.println("Number is even: "+(number % 2 == 0));
+        System.out.println("Number is odd: "+(number % 2 != 0));
     }
 }
