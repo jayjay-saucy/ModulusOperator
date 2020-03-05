68575d6d7132db5cd8e0c2361c98628c9e2ace07
Jayllian Marquardt <jayllian.marquardt.2021@student.lhschools.org>
2020-03-05 13:44:57 -0500

    lastDigitMethodCreated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,16 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
+    public static void lastDigit(){
+        lastDigit();
+    }
     public static void keepContained(){
         keepContained();
     }
 
     public static void testOdness(){
         testOdness();
+
 
     }
     public static void main(String[] args) {
@@ -20,39 +24,15 @@ public class Main {
         // is not even !=
 Scanner keyboard;
     keyboard = new Scanner(System.in);
-    int number =4;
-
+    int number =3421;
+    System.out.println("give me a number in the thousands");
+    keyboard.nextLine();
+System.out.println("The last digit is: " +number%10);
+number = number /10;
+System.out.println("The second too last digit is: " +number%10);
 
         // want to keep number contained
-        System.out.println("give me a number between 1-5");
-        keyboard.nextLine();
-        System.out.println("number contained is: Modulous Operation number is" + number);
-         number = 0;
-        System.out.println((number%5)+1);
 
-         number = 1;
-        System.out.println((number%5)+1);
-
-         number = 2;
-        System.out.println((number%5)+1);
-
-         number = 3;
-        System.out.println((number%5)+1);
-
-         number = 4;
-        System.out.println((number%5)+1);
-
-         number = 5;
-        System.out.println((number%5)+1);
-
-        number = 6;
-        System.out.println((number%5)+1);
-
-        number = 7;
-        System.out.println((number%5)+1);
-
-        number = 8;
-        System.out.println((number%5)+1);
 
 
     }
